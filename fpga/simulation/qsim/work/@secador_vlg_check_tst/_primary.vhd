f0bc7d52998a06eebd57116f767b17d9c0d1ec78
51rL1N5 <lipeols@hotmail.com>
2018-11-23 23:13:33 -0300

    Commit final -- deu certo

---
+++ fpga/simulation/qsim/work/@secador_vlg_check_tst/_primary.vhd
@@ -0,0 +1,8 @@
+library verilog;
+use verilog.vl_types.all;
+entity Secador_vlg_check_tst is
+    port(
+        DATA_MISO       : in     vl_logic;
+        sampler_rx      : in     vl_logic
+    );
+end Secador_vlg_check_tst;
