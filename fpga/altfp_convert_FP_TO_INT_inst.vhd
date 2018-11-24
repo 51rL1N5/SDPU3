45e1e3fe0eb0238691de72a46cda7ef5e4ad61a5
51rL1N5 <lipeols@hotmail.com>
2018-11-23 23:12:30 -0300

    wipe

--- fpga/altfp_convert_FP_TO_INT_inst.vhd
+++
@@ -1,5 +0,0 @@
-altfp_convert_FP_TO_INT_inst : altfp_convert_FP_TO_INT PORT MAP (
-		clock	 => clock_sig,
-		dataa	 => dataa_sig,
-		result	 => result_sig
-	);
--- fpga/altfp_convert_INT_TO_FP_inst.vhd
+++
@@ -1,5 +0,0 @@
-altfp_convert_INT_TO_FP_inst : altfp_convert_INT_TO_FP PORT MAP (
-		clock	 => clock_sig,
-		dataa	 => dataa_sig,
-		result	 => result_sig
-	);
